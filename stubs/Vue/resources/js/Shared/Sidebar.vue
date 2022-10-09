2d40fcbf2e42b138c2204db7da782bf180ead663
Edo Pranata <edo.pranata@gmail.com>
2022-10-09 12:39:26 +0700

    Fix some class in sidebar

--- stubs/Vue/resources/js/Shared/Sidebar.vue
+++ stubs/Vue/resources/js/Shared/Sidebar.vue
@@ -2,8 +2,8 @@
     <div class="drawer-side pt-0">
         <label for="my-drawer-2" class="drawer-overlay"></label>
         <ul class="menu overflow-y-auto w-80 text-base-content bg-base-200">
-            <li class="bg-primary/75 text-base-100">
-                <div class="navbar h-20 hover:border-primary hover:border-b-2">
+            <li class="bg-primary/75 text-base-100 hover:bg-primary/100 transition-all">
+                <div class="navbar h-20 border-primary border-b-2">
                     <a class="normal-case text-xl">RiauCoder's</a>
                 </div>
             </li>
